43d0798a76c8b0c6062e585fb856adb69ef26981
Mathilde Dupont <dupont.mathilde.d@gmail.com>
2019-04-01 21:00:36 +0000

    doing cocktails pages

---
+++ application.js
@@ -0,0 +1,17 @@
+$(".shaker-button button").click(function(){
+  $(".shaker img").effect( "shake", {times:6, direction:'up'}, 1500 );
+});
+
+$(".ingredients img").hover(function(){
+  $(".ingredients img").not(this).addClass("not-active");
+}, function() {
+  $(".ingredients img").removeClass("not-active");
+});
+
+$(".ingredients img").hover(function(){
+  var elementId = $(this).attr('id')
+  $(".ingredients #" + elementId + "-tooltip").addClass("visible");
+}, function() {
+  var elementId = $(this).attr('id')
+  $(".ingredients #" + elementId + "-tooltip").removeClass("visible");
+});
